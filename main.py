0b1c11681abfac61a7d155b491bda6004c342bd1
hautist <lookatnadeko@protonmail.com>
2025-06-09 11:46:45 +0100

    task 4: limit framerate with pygame clock

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0  # delta time
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("Black")
         pygame.display.flip()
+
+        # limit framerate to 60 (and covert dt from ms to seconds)
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
