12faa076a8754faa1cbae86318e64042834d51af
hautist <lookatnadeko@protonmail.com>
2025-06-09 11:21:50 +0100

    task 2: define constants in second file and import to main file

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # this allows us to use code from pygame lib in this file
 import pygame
 
+# import everything from constants.py
+from constants import *
+
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
